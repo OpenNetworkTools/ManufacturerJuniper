67b39c47a342f71831060487cc70b433d4f02dd7
Thibault CHEVALLERAUD <tchevalleraud@interdata.fr>
2019-01-13 09:15:18 +0100

    update Juniper

---
+++ src/QFX.php
@@ -0,0 +1,11 @@
+<?php
+    namespace OpenNetworkTools\Manufacturer\Juniper;
+        
+    class QFX extends \OpenNetworkTools\Manufacturer {
+
+        public function __construct(){
+            parent::__construct();
+        }
+    
+    }
+?>
---
+++ src/QFX/QFX5100.php
@@ -0,0 +1,10 @@
+<?php
+    namespace OpenNetworkTools\Manufacturer\Juniper\QFX;
+        
+    class QFX5100 extends \OpenNetworkTools\Manufacturer\Juniper\QFX {
+    
+        public function __construct(){
+            parent::__construct();
+        }
+
+    }
